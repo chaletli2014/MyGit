07067ce44b138184d7fbead9bbaee79ea54a1602
chaletli2014 <chaletli@163.com>
2014-09-11 22:37:10 +0800

    添加最近几周周报数据

--- LSKPI/WebContent/WEB-INF/database/PED_weekly_data.sql
+++ LSKPI/WebContent/WEB-INF/database/PED_weekly_data.sql
@@ -3,6 +3,146 @@ truncate table tbl_pediatrics_data_weekly;
 insert into tbl_pediatrics_data_weekly 
 select 
 null,
+CONCAT(DATE_FORMAT(DATE_SUB('2014-09-08', Interval 7 day),'%Y.%m.%d'), '-',DATE_FORMAT(DATE_SUB('2014-09-08', Interval 1 day),'%Y.%m.%d')) as duration, 
+h.name,
+h.code,
+pd_data.inNum,
+pd_data.pnum,
+pd_data.whnum,
+pd_data.lsnum,
+pd_data.averageDose,
+pd_data.hmgRate,
+pd_data.omgRate,
+pd_data.tmgRate,
+pd_data.fmgRate,
+h.saleCode,
+h.dsmCode,
+h.rsmRegion,
+h.region,
+now() 
+from (
+SELECT 
+	h.code,	
+	count_hos.inNum,
+	(sum(pd.pnum)/count_hos.inNum)*5 as pnum,
+	(sum(pd.whnum)/count_hos.inNum)*5 as whnum,
+	(sum(pd.lsnum)/count_hos.inNum)*5 as lsnum,
+	IFNULL( 
+				sum( 
+					( 
+						( 0.5*IFNULL(pd.hqd,0) 
+						+ 0.5*2*IFNULL(pd.hbid,0) 
+						+ 1*1*IFNULL(pd.oqd,0) 
+						+ 1*2*IFNULL(pd.obid,0) 
+						+ 2*1*IFNULL(pd.tqd,0) 
+						+ 2*2*IFNULL(pd.tbid,0) 
+						) / 100 
+					) * IFNULL(pd.lsnum,0) 
+				) / IFNULL(sum(pd.lsnum),0)
+			,0 ) averageDose, 
+	IFNULL(
+		sum(IFNULL(pd.hqd,0)*pd.lsnum/100)/sum(pd.lsnum),0
+	) hmgRate,
+	IFNULL(
+		sum((IFNULL(pd.hbid,0)*pd.lsnum + IFNULL(pd.oqd,0)*pd.lsnum)/100)/sum(pd.lsnum),0
+	) omgRate,
+	IFNULL(
+		sum((IFNULL(pd.obid,0)*pd.lsnum + IFNULL(pd.tqd,0)*pd.lsnum)/100)/sum(pd.lsnum),0
+	) tmgRate,
+	IFNULL(
+		sum(IFNULL(pd.tbid,0)*pd.lsnum/100)/sum(pd.lsnum),0
+	) fmgRate 
+    FROM tbl_pediatrics_data pd, tbl_hospital h, 
+	(
+		select count(1) as inNum, h.code 
+		from tbl_pediatrics_data pd, tbl_hospital h
+		WHERE pd.createdate between DATE_SUB('2014-09-08', Interval 7 day) and '2014-09-08' 
+		and pd.hospitalName = h.name 
+		and h.isPedAssessed='1' 
+		GROUP BY h.code
+	) count_hos 
+    WHERE pd.createdate between DATE_SUB('2014-09-08', Interval 7 day) and '2014-09-08' 
+	and pd.hospitalName = h.name 
+	and h.code = count_hos.code
+	and h.isPedAssessed='1' 
+    GROUP BY h.code
+) pd_data 
+right join tbl_hospital h on pd_data.code = h.code 
+where h.isPedAssessed='1';
+
+insert into tbl_pediatrics_data_weekly 
+select 
+null,
+CONCAT(DATE_FORMAT(DATE_SUB('2014-09-01', Interval 7 day),'%Y.%m.%d'), '-',DATE_FORMAT(DATE_SUB('2014-09-01', Interval 1 day),'%Y.%m.%d')) as duration, 
+h.name,
+h.code,
+pd_data.inNum,
+pd_data.pnum,
+pd_data.whnum,
+pd_data.lsnum,
+pd_data.averageDose,
+pd_data.hmgRate,
+pd_data.omgRate,
+pd_data.tmgRate,
+pd_data.fmgRate,
+h.saleCode,
+h.dsmCode,
+h.rsmRegion,
+h.region,
+now() 
+from (
+SELECT 
+	h.code,	
+	count_hos.inNum,
+	(sum(pd.pnum)/count_hos.inNum)*5 as pnum,
+	(sum(pd.whnum)/count_hos.inNum)*5 as whnum,
+	(sum(pd.lsnum)/count_hos.inNum)*5 as lsnum,
+	IFNULL( 
+				sum( 
+					( 
+						( 0.5*IFNULL(pd.hqd,0) 
+						+ 0.5*2*IFNULL(pd.hbid,0) 
+						+ 1*1*IFNULL(pd.oqd,0) 
+						+ 1*2*IFNULL(pd.obid,0) 
+						+ 2*1*IFNULL(pd.tqd,0) 
+						+ 2*2*IFNULL(pd.tbid,0) 
+						) / 100 
+					) * IFNULL(pd.lsnum,0) 
+				) / IFNULL(sum(pd.lsnum),0)
+			,0 ) averageDose, 
+	IFNULL(
+		sum(IFNULL(pd.hqd,0)*pd.lsnum/100)/sum(pd.lsnum),0
+	) hmgRate,
+	IFNULL(
+		sum((IFNULL(pd.hbid,0)*pd.lsnum + IFNULL(pd.oqd,0)*pd.lsnum)/100)/sum(pd.lsnum),0
+	) omgRate,
+	IFNULL(
+		sum((IFNULL(pd.obid,0)*pd.lsnum + IFNULL(pd.tqd,0)*pd.lsnum)/100)/sum(pd.lsnum),0
+	) tmgRate,
+	IFNULL(
+		sum(IFNULL(pd.tbid,0)*pd.lsnum/100)/sum(pd.lsnum),0
+	) fmgRate 
+    FROM tbl_pediatrics_data pd, tbl_hospital h, 
+	(
+		select count(1) as inNum, h.code 
+		from tbl_pediatrics_data pd, tbl_hospital h
+		WHERE pd.createdate between DATE_SUB('2014-09-01', Interval 7 day) and '2014-09-01' 
+		and pd.hospitalName = h.name 
+		and h.isPedAssessed='1' 
+		GROUP BY h.code
+	) count_hos 
+    WHERE pd.createdate between DATE_SUB('2014-09-01', Interval 7 day) and '2014-09-01' 
+	and pd.hospitalName = h.name 
+	and h.code = count_hos.code
+	and h.isPedAssessed='1' 
+    GROUP BY h.code
+) pd_data 
+right join tbl_hospital h on pd_data.code = h.code 
+where h.isPedAssessed='1';
+
+insert into tbl_pediatrics_data_weekly 
+select 
+null,
 CONCAT(DATE_FORMAT(DATE_SUB('2014-08-25', Interval 7 day),'%Y.%m.%d'), '-',DATE_FORMAT(DATE_SUB('2014-08-25', Interval 1 day),'%Y.%m.%d')) as duration, 
 h.name,
 h.code,
--- LSKPI/WebContent/WEB-INF/database/RES_weekly_data.sql
+++ LSKPI/WebContent/WEB-INF/database/RES_weekly_data.sql
@@ -3,6 +3,168 @@ truncate table tbl_respirology_data_weekly;
 insert into tbl_respirology_data_weekly 
 select 
 null,
+CONCAT(DATE_FORMAT(DATE_SUB('2014-09-08', Interval 7 day),'%Y.%m.%d'), '-',DATE_FORMAT(DATE_SUB('2014-09-08', Interval 1 day),'%Y.%m.%d')) as duration, 
+h.name,
+h.code,
+rd_data.inNum,
+rd_data.pnum,
+rd_data.whnum,
+rd_data.aenum,
+rd_data.lsnum,
+rd_data.averageDose,
+rd_data.omgRate,
+rd_data.tmgRate,
+rd_data.thmgRate,
+rd_data.fmgRate,
+rd_data.smgRate,
+rd_data.emgRate,
+h.saleCode,
+h.dsmCode,
+h.rsmRegion,
+h.region,
+now(),Year(DATE_SUB('2014-09-08', Interval 7 day)),Month(DATE_SUB('2014-09-08', Interval 7 day))  
+from (
+SELECT 
+    h.code, 
+    count_hos.inNum,
+    (sum(rd.pnum)/count_hos.inNum)*5 as pnum,
+    (sum(rd.aenum)/count_hos.inNum)*5 as aenum,
+    (sum(rd.whnum)/count_hos.inNum)*5 as whnum,
+    (sum(rd.lsnum)/count_hos.inNum)*5 as lsnum,
+    IFNULL( 
+                sum( 
+                    ( 
+                        ( 1*IFNULL(rd.oqd,0) 
+                        + 2*1*IFNULL(rd.tqd,0) 
+                        + 1*3*IFNULL(rd.otid,0) 
+                        + 2*2*IFNULL(rd.tbid,0) 
+                        + 2*3*IFNULL(rd.ttid,0) 
+                        + 3*2*IFNULL(rd.thbid,0) 
+                        + 4*2*IFNULL(rd.fbid,0) 
+                        ) / 100  
+                    ) * IFNULL(rd.lsnum,0) 
+                ) / IFNULL(sum(rd.lsnum),0)
+            ,0 ) averageDose, 
+    IFNULL(
+        sum(IFNULL(rd.oqd,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) omgRate,
+    IFNULL(
+        sum(IFNULL(rd.tqd,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) tmgRate,
+    IFNULL(
+        sum(IFNULL(rd.otid,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) thmgRate,
+    IFNULL(
+        sum(IFNULL(rd.tbid,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) fmgRate, 
+    IFNULL(
+        sum((IFNULL(rd.ttid,0)*rd.lsnum + IFNULL(rd.thbid,0)*rd.lsnum)/100)/sum(rd.lsnum),0
+    ) smgRate, 
+    IFNULL(
+        sum(IFNULL(rd.fbid,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) emgRate 
+    FROM tbl_respirology_data rd, tbl_hospital h, 
+    (
+        select count(1) as inNum, h.code 
+        from tbl_respirology_data rd, tbl_hospital h
+        WHERE rd.createdate between DATE_SUB('2014-09-08', Interval 7 day) and '2014-09-08' 
+        and rd.hospitalName = h.name 
+        and h.isResAssessed='1' 
+        GROUP BY h.code
+    ) count_hos 
+    WHERE rd.createdate between DATE_SUB('2014-09-08', Interval 7 day) and '2014-09-08' 
+    and rd.hospitalName = h.name 
+    and h.code = count_hos.code
+    and h.isResAssessed='1' 
+    GROUP BY h.code
+) rd_data 
+right join tbl_hospital h on rd_data.code = h.code 
+where h.isResAssessed='1';
+
+insert into tbl_respirology_data_weekly 
+select 
+null,
+CONCAT(DATE_FORMAT(DATE_SUB('2014-09-01', Interval 7 day),'%Y.%m.%d'), '-',DATE_FORMAT(DATE_SUB('2014-09-01', Interval 1 day),'%Y.%m.%d')) as duration, 
+h.name,
+h.code,
+rd_data.inNum,
+rd_data.pnum,
+rd_data.whnum,
+rd_data.aenum,
+rd_data.lsnum,
+rd_data.averageDose,
+rd_data.omgRate,
+rd_data.tmgRate,
+rd_data.thmgRate,
+rd_data.fmgRate,
+rd_data.smgRate,
+rd_data.emgRate,
+h.saleCode,
+h.dsmCode,
+h.rsmRegion,
+h.region,
+now(),Year(DATE_SUB('2014-09-01', Interval 7 day)),Month(DATE_SUB('2014-09-01', Interval 7 day))  
+from (
+SELECT 
+    h.code, 
+    count_hos.inNum,
+    (sum(rd.pnum)/count_hos.inNum)*5 as pnum,
+    (sum(rd.aenum)/count_hos.inNum)*5 as aenum,
+    (sum(rd.whnum)/count_hos.inNum)*5 as whnum,
+    (sum(rd.lsnum)/count_hos.inNum)*5 as lsnum,
+    IFNULL( 
+                sum( 
+                    ( 
+                        ( 1*IFNULL(rd.oqd,0) 
+                        + 2*1*IFNULL(rd.tqd,0) 
+                        + 1*3*IFNULL(rd.otid,0) 
+                        + 2*2*IFNULL(rd.tbid,0) 
+                        + 2*3*IFNULL(rd.ttid,0) 
+                        + 3*2*IFNULL(rd.thbid,0) 
+                        + 4*2*IFNULL(rd.fbid,0) 
+                        ) / 100  
+                    ) * IFNULL(rd.lsnum,0) 
+                ) / IFNULL(sum(rd.lsnum),0)
+            ,0 ) averageDose, 
+    IFNULL(
+        sum(IFNULL(rd.oqd,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) omgRate,
+    IFNULL(
+        sum(IFNULL(rd.tqd,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) tmgRate,
+    IFNULL(
+        sum(IFNULL(rd.otid,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) thmgRate,
+    IFNULL(
+        sum(IFNULL(rd.tbid,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) fmgRate, 
+    IFNULL(
+        sum((IFNULL(rd.ttid,0)*rd.lsnum + IFNULL(rd.thbid,0)*rd.lsnum)/100)/sum(rd.lsnum),0
+    ) smgRate, 
+    IFNULL(
+        sum(IFNULL(rd.fbid,0)*rd.lsnum/100)/sum(rd.lsnum),0
+    ) emgRate 
+    FROM tbl_respirology_data rd, tbl_hospital h, 
+    (
+        select count(1) as inNum, h.code 
+        from tbl_respirology_data rd, tbl_hospital h
+        WHERE rd.createdate between DATE_SUB('2014-09-01', Interval 7 day) and '2014-09-01' 
+        and rd.hospitalName = h.name 
+        and h.isResAssessed='1' 
+        GROUP BY h.code
+    ) count_hos 
+    WHERE rd.createdate between DATE_SUB('2014-09-01', Interval 7 day) and '2014-09-01' 
+    and rd.hospitalName = h.name 
+    and h.code = count_hos.code
+    and h.isResAssessed='1' 
+    GROUP BY h.code
+) rd_data 
+right join tbl_hospital h on rd_data.code = h.code 
+where h.isResAssessed='1';
+
+insert into tbl_respirology_data_weekly 
+select 
+null,
 CONCAT(DATE_FORMAT(DATE_SUB('2014-08-25', Interval 7 day),'%Y.%m.%d'), '-',DATE_FORMAT(DATE_SUB('2014-08-25', Interval 1 day),'%Y.%m.%d')) as duration, 
 h.name,
 h.code,
